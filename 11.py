785bf95d05d1f16582d88a93dd83ace3a6bb4c89
Shaik.Nagursharif <sknagursharif@outlook.com>
2021-11-05 21:02:12 +0530

    My first dedication

---
+++ 11.py
@@ -0,0 +1,18 @@
+# .....................................................................Packages Import..................................................
+from tkinter import *
+from tkinter import messagebox as mb
+from PIL import Image, ImageTk
+
+import csv
+from openpyxl.worksheet import worksheet
+import sqlite3
+import smtplib
+import pandas as pd
+from email.message import EmailMessage
+import re
+import random
+import pandas as pd
+from win32.win32api import GetSystemMetrics
+from tkscrolledframe import ScrolledFrame
+from tkinter import PhotoImage
+import matplotlib.pyplot as plt
